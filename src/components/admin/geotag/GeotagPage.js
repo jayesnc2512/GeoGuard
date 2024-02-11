6c8f85f75ae816109b31e85af59aa2ec7c68bbd4
Savio629 <diassavio629@gmail.com>
2024-02-11 12:46:45 +0530

    reverted a file

--- src/components/admin/geotag/GeotagPage.js
+++ src/components/admin/geotag/GeotagPage.js
@@ -1,8 +1,9 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import MapWrapper from './leaflet';
-import { useAuthContext } from '../../../hooks/useAuthContext';
-import Gmaps from './gmaps';
+import Gmaps from './gmaps.js';
+import Nav from '../../nav/Nav';
+import { useAuthContext } from '../../../hooks/useAuthContext.js';
+
 const GeotagPageWrapper = styled.div`
   .leaflet-container {
     margin: 30px auto;
@@ -26,38 +27,38 @@ const GeotagPage = () => {
   const { userData } = useAuthContext();
 
 
-const getLocations = (async () => {
+  const getLocations = (async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/camera/getCamByUserId/`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/camera/getAllCamera/`, {
         method: 'GET',
         headers: {
           'token': userData.token,
-          'userId': userData.user._id,
         },
       });
       if (response.ok) {
         const camerasList = await response.json();
-        console.log(camerasList);
-        setCameras(camerasList.camByUser);
-        const mappedCoordinates = camerasList.camByUser.map((cam) => ({
+        //console.log(camerasList.allCamera);
+        setCameras(camerasList.allCamera);
+        const mappedCoordinates = camerasList.allCamera.map((cam) => ({
           lat: cam.location.lat,
           lng: cam.location.lon,
           lid: cam.licenseId,
-          ip:cam.ip,
-          imgs: cam.pictures.pic1
+          ip:cam.ip
         }));
+        
 
         setCoordinates([...coordinates, ...mappedCoordinates]);
+
       }
     } catch (err) {
       console.warn("getCameras failed", err);
     }
-    console.log("coordinates1",coordinates);
   })
 
   useEffect(() => {
     getLocations();
   }, [userData]);
+
 
   const handleCoordinateChange = (lat, lng) => {
     setCoordinates([...coordinates, { lat, lng }]); //city nikala
@@ -68,26 +69,39 @@ const getLocations = (async () => {
 
   return (
     <GeotagPageWrapper>
+      {/* <Nav /> */}
+
       <div className="geotag">
-        <div>
-          <h2>View Geotags</h2>
-          {/* <label>
-            Latitude:
-            <input
-              type="text"
-              value={newLat}
-              onChange={(e) => setNewLat(e.target.value)}
-            />
-          </label>
-          <label>
-            Longitude:
-            <input
-              type="text"
-              value={newLng}
-              onChange={(e) => setNewLng(e.target.value)}
-            />
-          </label> */}
-          {/* <label>
+       
+        <Gmaps coordinates={coordinates} />
+      </div>
+    </GeotagPageWrapper>
+  );
+};
+
+export default GeotagPage;
+
+
+
+{/* <div>
+  <h2>Add Coordinates</h2>
+  <label>
+    Latitude:
+    <input
+      type="text"
+      value={newLat}
+      onChange={(e) => setNewLat(e.target.value)}
+    />
+  </label>
+  <label>
+    Longitude:
+    <input
+      type="text"
+      value={newLng}
+      onChange={(e) => setNewLng(e.target.value)}
+    />
+  </label>
+  {/* <label>
             City:
             <input
               type="text"
@@ -95,20 +109,11 @@ const getLocations = (async () => {
               onChange={(e) => setCity(e.target.value)}
             />
           </label> */}
-          {/* <button
-            onClick={() =>
-              handleCoordinateChange(parseFloat(newLat), parseFloat(newLng))
-            }
-          >
-            Add Coordinate
-          </button> */}
-        </div>
-
-        {/* <MapWrapper coordinates={coordinates} /> */}
-        <Gmaps coordinates={coordinates}/>
-      </div>
-    </GeotagPageWrapper>
-  );
-};
-
-export default GeotagPage;
+  // <button
+//     onClick={() =>
+//       handleCoordinateChange(parseFloat(newLat), parseFloat(newLng))
+//     }
+//   >
+//     Add Coordinate
+//   </button>
+// </div> */}
