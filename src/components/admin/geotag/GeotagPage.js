0f2d8b3d38d67a66b3736e202fb74c74ab38b468
Savio629 <diassavio629@gmail.com>
2024-02-11 12:41:14 +0530

    gmaps

--- src/components/admin/geotag/GeotagPage.js
+++ src/components/admin/geotag/GeotagPage.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import styled from 'styled-components';
 import MapWrapper from './leaflet';
-import Nav from '../../nav/Nav';
-import { useAuthContext } from '../../../hooks/useAuthContext.js';
-
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import Gmaps from './gmaps';
 const GeotagPageWrapper = styled.div`
   .leaflet-container {
     margin: 30px auto;
@@ -27,38 +26,38 @@ const GeotagPage = () => {
   const { userData } = useAuthContext();
 
 
-  const getLocations = (async () => {
+const getLocations = (async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/camera/getAllCamera/`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/camera/getCamByUserId/`, {
         method: 'GET',
         headers: {
           'token': userData.token,
+          'userId': userData.user._id,
         },
       });
       if (response.ok) {
         const camerasList = await response.json();
-        //console.log(camerasList.allCamera);
-        setCameras(camerasList.allCamera);
-        const mappedCoordinates = camerasList.allCamera.map((cam) => ({
+        console.log(camerasList);
+        setCameras(camerasList.camByUser);
+        const mappedCoordinates = camerasList.camByUser.map((cam) => ({
           lat: cam.location.lat,
           lng: cam.location.lon,
           lid: cam.licenseId,
-          ip:cam.ip
+          ip:cam.ip,
+          imgs: cam.pictures.pic1
         }));
-        
 
         setCoordinates([...coordinates, ...mappedCoordinates]);
-
       }
     } catch (err) {
       console.warn("getCameras failed", err);
     }
+    console.log("coordinates1",coordinates);
   })
 
   useEffect(() => {
     getLocations();
   }, [userData]);
-
 
   const handleCoordinateChange = (lat, lng) => {
     setCoordinates([...coordinates, { lat, lng }]); //city nikala
@@ -69,39 +68,26 @@ const GeotagPage = () => {
 
   return (
     <GeotagPageWrapper>
-      {/* <Nav /> */}
-
       <div className="geotag">
-       
-        <MapWrapper coordinates={coordinates} />
-      </div>
-    </GeotagPageWrapper>
-  );
-};
-
-export default GeotagPage;
-
-
-
-{/* <div>
-  <h2>Add Coordinates</h2>
-  <label>
-    Latitude:
-    <input
-      type="text"
-      value={newLat}
-      onChange={(e) => setNewLat(e.target.value)}
-    />
-  </label>
-  <label>
-    Longitude:
-    <input
-      type="text"
-      value={newLng}
-      onChange={(e) => setNewLng(e.target.value)}
-    />
-  </label>
-  {/* <label>
+        <div>
+          <h2>View Geotags</h2>
+          {/* <label>
+            Latitude:
+            <input
+              type="text"
+              value={newLat}
+              onChange={(e) => setNewLat(e.target.value)}
+            />
+          </label>
+          <label>
+            Longitude:
+            <input
+              type="text"
+              value={newLng}
+              onChange={(e) => setNewLng(e.target.value)}
+            />
+          </label> */}
+          {/* <label>
             City:
             <input
               type="text"
@@ -109,11 +95,20 @@ export default GeotagPage;
               onChange={(e) => setCity(e.target.value)}
             />
           </label> */}
-  // <button
-//     onClick={() =>
-//       handleCoordinateChange(parseFloat(newLat), parseFloat(newLng))
-//     }
-//   >
-//     Add Coordinate
-//   </button>
-// </div> */}
+          {/* <button
+            onClick={() =>
+              handleCoordinateChange(parseFloat(newLat), parseFloat(newLng))
+            }
+          >
+            Add Coordinate
+          </button> */}
+        </div>
+
+        {/* <MapWrapper coordinates={coordinates} /> */}
+        <Gmaps coordinates={coordinates}/>
+      </div>
+    </GeotagPageWrapper>
+  );
+};
+
+export default GeotagPage;
